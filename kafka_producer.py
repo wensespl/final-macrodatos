98024b8110a318b1a25bb1e4245c453deafc28bb
wensespl <wenses400@gmail.com>
2024-07-02 23:24:05 -0500

    UPDATE file

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,14 +1,13 @@
 from confluent_kafka import Producer
+import time
+import csv
 
 # Kafka broker configuration
 bootstrap_servers = 'localhost:9092'
+topic = 'streaming_topic'
 
 # Create a Kafka producer
 producer = Producer({'bootstrap.servers': bootstrap_servers})
-
-# Example topic and message
-topic = 'streaming_topic'
-message = 'erghejstrfjseh'
 
 # Delivery report callback
 def delivery_report(err, msg):
@@ -17,8 +16,14 @@ def delivery_report(err, msg):
     else:
         print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
 
-# Produce a message asynchronously
-producer.produce(topic, message.encode('utf-8'), callback=delivery_report)
+# Read data from CSV and send to Kafka
+with open('datos_atria_final.csv', 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader) # Ignoar la primera fila
+    for row in csvreader:
+        message = ','.join(row)
+        producer.produce(topic, message.encode('utf-8'), callback=delivery_report)
+        time.sleep(1)
 
 # Wait for all messages to be delivered
 producer.flush()
